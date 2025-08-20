c4b45e97a34f721d08d3135278598bd8f17e8bbd
phzxlssd-dev <nnascimentop@gmail.com>
2025-08-20 20:02:02 -0300

    Update atividade1.cpp

--- atividades/atividade1.cpp
+++ atividades/atividade1.cpp
@@ -1,63 +1,38 @@
 #include <iostream>
-#include <vector>
 #include <string>
-
-class Book {
+#include <vector>
+using namespace std;
+class book {
 public:
-    std::string title;
-    std::string author;
-    int year;
+string titulo;
+string autor;
+            int anos;
 
-    Book(std::string t, std::string a, int y)
-        : title(t), author(a), year(y) {}
-    
-    void print() {
-        std::cout << title << " (" << year << "), por " << author << std::endl;
-    }
+
 };
-
-class User {
-public:
-    std::string nome;
-    std::string endereco;
-    int idade;
-    std::vector<Book> livros_alugados;
-
-    User(std::string n, std::string e, int i, std::vector<Book> livros)
-        : nome(n), endereco(e), idade(i), livros_alugados(livros) {}
-
-    void print() {
-        std::cout << "Nome: " << nome << std::endl;
-        std::cout << "Endereço: " << endereco << std::endl;
-        std::cout << "Idade: " << idade << std::endl;
-        std::cout << "Livros alugados:" << std::endl;
-        if (livros_alugados.empty()) {
-            std::cout << "  Nenhum" << std::endl;
-        } else {
-            for (auto &livro : livros_alugados) {
-                std::cout << "  - ";
-                livro.print();
-            }
-        }
-        std::cout << std::endl;
-    }
+class  user    {
+ public:
+string nome;
+string endereco;
+        int idade;
+     
 };
+int main()  {
+   book mybook;
+   mybook.titulo = "O senhor dos aneis";
+   mybook.autor = "J.R.R. Tolkien";
+   mybook.anos = 1954;
+   
+   user userinfo;
+        userinfo.nome = "Pedro Henrique Nunes";
+        userinfo.endereco = "Rua das Flores, 123";
+        userinfo.idade = 23;
+        cout << "Livro:" << endl;
+    cout << "Título: " << mybook.titulo << endl;
+    cout << "Autor: " << mybook.autor << endl;
+    cout << "Ano: " << mybook.anos << endl;
 
-int main() {
-    // Criando alguns livros
-    Book livro1("Dom Casmurro", "Machado de Assis", 1899);
-    Book livro2("O Senhor dos Anéis", "J. R. R. Tolkien", 1954);
-
-    // Criando usuários
-    User usuario1("Ana Silva", "Rua A, 123", 25, {livro1});
-    User usuario2("Carlos Souza", "Av. B, 456", 30, {livro1, livro2});
-
-    // Imprimindo atributos dos usuários
-    std::cout << "Usuário 1:" << std::endl;
-    usuario1.print();
-
-    std::cout << "Usuário 2:" << std::endl;
-    usuario2.print();
-
-    return 0;
-}
+    cout << "\nUsuário:" << endl;
+    cout << "Nome: " << userinfo.nome << endl;
+    cout << "Endereço: " << userinfo.endereco << endl;
+    cout << "Idade: " << userinfo.idade << endl;
