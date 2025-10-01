8f6bce17251945080e78d6df902d7aa5aecb94ec
phzxlssd-dev <nnascimentop@gmail.com>
2025-10-01 20:13:46 -0300

    Update atividade0.py

--- atividades/atividade0.py
+++ atividades/atividade0.py
@@ -1,30 +1,31 @@
 class Book:
-    def __init__(self, titulo, author, ano):
+    def __init__(self, titulo:str, author:str, ano:int):
         self.titulo = titulo
         self.author = author
         self.ano = ano
-
-    def __str__(self):
-        return f'"{self.titulo}" por {self.author} ({self.ano})'
-
-
+        p1=(p1)
 class User:
-    def __init__(self, nome, endereco, idade, livros_alugados=None):
+    def __init__(self, nome:str, endereco:str, idade:int ):
         self.nome = nome
         self.endereco = endereco
         self.idade = idade
-        self.livros_alugados = livros_alugados if livros_alugados else []
+        self.livros_alugados =[]
 
-    def __str__(self):
-        livros = ', '.join(str(livro) for livro in self.livros_alugados) if self.livros_alugados else "Nenhum livro alugado"
-        return f'Nome: {self.nome}\nEndereço: {self.endereco}\nIdade: {self.idade}\nLivros alugados: {livros}'
+    def __alugar_livro(self, livro:Book):
+        self . livros_alugados.append(livro)
+ 
+    def __remover_livro(self, livro:Book):
+        self.livros_alugados.remove(livro)
 
+    def __imprimir_livros(self):
+        for livro in self.livros_alugados:
+            print(livro.titulo)
+livro1= Book("planet of monkeys","pierre",1968)
+livro2= Book("castle inter","golber",2022)
 
-
-p1 = Book("Planeta dos Macacos", "Pierre Boulle", 1968)
-
-
-usuario1 = User("João da Silva", "Rua A, 123", 30, [p1])
-
-
-print(usuario1)
+user1= User("pateta","rua das rosas",22)
+user2= User("bobo","rua das carlas",21)
+user1.alugar_livro(livro1)
+user2.alugar_livro(livro2)
+user1.imprimir_livros(livro1)
+user2.imprimir_livros(livro2)
